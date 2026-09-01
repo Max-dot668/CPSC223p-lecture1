c64e18dd409abc4b18af83d0d4f1e2cafbd8bfca
Max You <maxiyouit@gmail.com>
2026-09-01 16:29:18 +0000

    Add inclassvariables.py with variable examples and user input

--- M3A1.py
+++ M3A1.py
@@ -2,5 +2,9 @@
 # Student ID: 123456
 # Section: 87
 # Assignment: Module 3 Assignment 1
+if __name__ == "__main__":
+    print("This is main")
+else:
+    print("Not main")
 
 print("Hello, world!")
---
+++ inclassvariables.py
@@ -0,0 +1,8 @@
+x = int(11 / 2)
+print(f"The number is {x ** 4}")
+
+a, b, c = 1, 2, 3
+print(f"a is {a} and b is {b} and c is {c}")
+
+name = input("What is your name? ")
+print(f"Your name is {name}")
